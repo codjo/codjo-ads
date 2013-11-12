fa57189895b361ded620c5e462397af8347fb4e8
marcona <arnaud.marconnet@codjo.net>
2013-11-12 14:45:47 +0100

    ads test account password change

--- src/test/java/net/codjo/ads/AdsTest.java
+++ src/test/java/net/codjo/ads/AdsTest.java
@@ -32,7 +32,7 @@ public interface AdsTest {
           .setPermissionBase(PERMISSION_BASE).get();
 
     String LOGIN = "frnormal1";
-    String PASSWORD = "adspwd*13";
+    String PASSWORD = "adspwd*14";
 
     int USER_SECURITY_LEVEL = 1;
     int SERVICE_SECURITY_LEVEL = 0;
